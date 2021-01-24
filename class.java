3a82d83a6c01b7d8f90cbdeb6497753a1a2b1786
raXXen <Bjar6492@web.de>
2021-01-24 18:04:42 +0100

    Eine neu Klasse wurde in einer eigenen Datei angelegt, die ersten Attribute wurden gesetzt, und Getter und Setter gechrieben

---
+++ class.java
@@ -0,0 +1,24 @@
+public class Klasse {
+
+    //attribute
+    private String Name = "Klasse";
+    private int age = 1;
+    
+    //getter und setter der attribute
+    public void setName (String namen) {
+        this.name = namen;
+    }
+
+    public String getName () {
+        return namen;
+    }
+
+    public void set age (int age) {
+        this.age = age;
+    }
+
+    public int getAge () {
+        return age;
+    }
+
+
